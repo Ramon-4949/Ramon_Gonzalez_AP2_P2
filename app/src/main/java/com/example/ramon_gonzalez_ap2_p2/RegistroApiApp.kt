df2655a3231d7819543d4d7615c60e5d7d72d37d
Ramon-Laptop <ramon_gonzalez2@ucne.edu.do>
2026-03-04 19:58:03 -0400

    Agregando componentes logicos necesarios

---
+++ app/src/main/java/com/example/ramon_gonzalez_ap2_p2/RegistroApiApp.kt
@@ -0,0 +1,6 @@
+package com.example.ramon_gonzalez_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+@HiltAndroidApp
+class RegistroApiApp : Application()
---
+++ app/src/main/java/com/example/ramon_gonzalez_ap2_p2/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.ramon_gonzalez_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+}
